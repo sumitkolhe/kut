05c6db0284f8c5d362ce67cb10de39db1edde930
sumit <thesumitkolhe@gmail.com>
2020-06-30 09:35:59 +0530

    :zap: updated to MVC architecture

---
+++ server/middleware/dbConnection.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+//Connect To Production Database
+if (process.env.NODE_ENV === "production") {
+  mongoose.connect(
+    process.env.MONGO_PROD_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    () => {
+      console.log("Connected to MongoDB Production -> links");
+    }
+  );
+} else if (process.env.NODE_ENV === "development") {
+  //Connect To Devlopment Database
+  mongoose.connect(
+    process.env.MONGO_DEV_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    () => {
+      console.log("Connected to MongoDB Development -> links");
+    }
+  );
+}
+
+
