b9bc54e79963e1dad6af778f215564acebb2aed5
sumit <thesumitkolhe@gmail.com>
2020-07-01 15:00:51 +0530

    :pencil2: typo fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const routes = require("./routes/reducedRoutes");
-
 const database = require("./middleware/dbConnection");
 require("dotenv").config();
 const app = express();
@@ -12,20 +11,18 @@ app.use(express.json());
 //Routes
 
 app.get("/api/status", (req, res) => {
-  res.status(200).json({ status: "OK"});
+  res.status(200).json({ status: "OK" });
 });
 
 app.use(routes);
 
-if (process.env.NODE_ENV === "development") {
-  app.use(express.static(__dirname + "/views/static/"));
-  app.get("/dashboard", (req, res) =>
-    res.sendFile(__dirname + "/views/static/index.html")
-  );
-  app.get(/.*/, (req, res) =>
-    res.sendFile(__dirname + "/views/errors/error.html")
-  );
-}
+app.use(express.static(__dirname + "/views/static/"));
+app.get("/dashboard", (req, res) =>
+  res.sendFile(__dirname + "/views/static/index.html")
+);
+app.get(/.*/, (req, res) =>
+  res.sendFile(__dirname + "/views/errors/error.html")
+);
 
 const port = process.env.PORT || 80;
 
