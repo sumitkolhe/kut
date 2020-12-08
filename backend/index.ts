b7cfac076ead3632fc3fe55bb51bd275d43585f4
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2020-12-08 22:06:42 +0530

    removed unecessary routes

--- backend/index.ts
+++ backend/index.ts
@@ -4,20 +4,13 @@ import route from "./routes/route";
 import { config } from "../config";
 import { connectDatabase } from "./utils/database";
 import { errorHandler } from "./middleware/error";
-import { verifyToken } from "./middleware/verifyToken";
 
 const app = express();
 connectDatabase();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.post("/", verifyToken, (req: express.Request, res: express.Response) => {
-  res.send(req.body);
-});
-
 app.use(route);
-
 app.use(
   (
     _req: express.Request,
@@ -27,7 +20,6 @@ app.use(
     next(createError(404, "Not Found"));
   }
 );
-
 app.use(errorHandler);
 
 app.listen(config.PORT, () =>
--- backend/services/controller/redirect.ts
+++ backend/services/controller/redirect.ts
@@ -2,18 +2,22 @@ import createError from "http-errors";
 import { RequestHandler } from "express";
 import { LinkModel } from "../../model/link.model";
 
-export const redirect: RequestHandler = async (req, _res, next) => {
-  let al: any = req.params.alias;
+export const redirect: RequestHandler = async (req, res, next) => {
+  let alias: any = req.params.alias;
   try {
-    const link = await LinkModel.findOne({
-      alias: al,
+    const linkDetails: any = await LinkModel.findOne({
+      alias: alias,
     });
 
-    if (!link) throw new createError.NotFound();
+    if (!linkDetails) throw new createError.NotFound();
 
-    link.clicks
-    
+    linkDetails.clicks++;
+    linkDetails.stats.push(Date());
+    linkDetails.markModified("stats");
+    await linkDetails.save();
+
+    res.redirect(301, linkDetails.longurl.toString());
   } catch (error) {
-    next();
+    next(error);
   }
 };
