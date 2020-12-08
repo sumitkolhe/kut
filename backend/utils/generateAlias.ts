94303111e8695f11edefabe33a4ce7b1d0ead9dd
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2020-12-08 21:50:53 +0530

    refak

--- backend/utils/generateAlias.ts
+++ backend/utils/generateAlias.ts
@@ -3,11 +3,11 @@ import { LinkModel } from "../model/link.model";
 
 export const generateUniqueAlias = async (): Promise<string> => {
   let randomAlias: any = "";
-  const aliasSuperSet = "abcdefghijklmnopqrstuvwxyz1234567890";
+  const characterSet = "abcdefghijklmnopqrstuvwxyz1234567890";
 
   for (var i = 0; i < config.ALIAS_LENGTH; i++) {
-    randomAlias += aliasSuperSet.charAt(
-      Math.floor(Math.random() * aliasSuperSet.length)
+    randomAlias += characterSet.charAt(
+      Math.floor(Math.random() * characterSet.length)
     );
   }
   var isAliasAvailable = await LinkModel.findOne({ alias: randomAlias });
