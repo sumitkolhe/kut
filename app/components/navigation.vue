7cf8482b36a30abd69a0ae2e0688fc6201f08261
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2020-12-10 22:32:08 +0530

    refak

---
+++ app/components/navigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar>
+    <h2>{{ appTitle }}</h2>
+    <v-spacer></v-spacer>
+
+    <v-btn icon large><v-icon> mdi-bell-ring-outline </v-icon></v-btn>
+
+    <v-btn icon large><v-icon> mdi-account-circle-outline </v-icon></v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return { appTitle: 'Reduced' }
+  },
+})
+</script>
+<style>
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <navigation />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style>
+
+</style>
