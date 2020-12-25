bd147d9ef18647de5aa3a3af6c8b9392beef3b78
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2020-12-26 00:15:52 +0530

    refak

--- backend/utils/verifyLink.ts
+++ backend/utils/verifyLink.ts
@@ -1,14 +1,14 @@
 import createError from "http-errors";
 
 export const verifyLink = (link: string) => {
-  const fullLinkPattern = /(?:(?:https?|ftp|file):\/\/|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim;
+  const linkWithProtocol = /(?:(?:https?|ftp|file):\/\/|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim;
 
-  const halfLinkPattern = /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,6})?(\/.*)?$/gim;
-  
-  //valid URL including http and domain
-  if (fullLinkPattern.test(link)) return link;
+  const linkWithoutProtocol = /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,6})?(\/.*)?$/gim;
+
+  //valid URL including http/https and domain
+  if (linkWithProtocol.test(link)) return link;
   //valid URL but http/https protocol not present
-  else if (halfLinkPattern.test(link)) return "https://" + link;
+  else if (linkWithoutProtocol.test(link)) return "https://" + link;
   //invalid URL
   else throw new createError.BadRequest("Invalid link");
 };
