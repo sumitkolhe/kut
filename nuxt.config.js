83de80bc51ef3c7fdab10f080313ab63cfdfe84c
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2020-12-28 23:27:22 +0530

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ module.exports = {
   server: {
     port: 80, // default: 3000
     host: '0.0.0.0', // default: localhost,
-  }
+  },
 
   buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
 
