004737935860f901c04cacd3cd33dfdf85333d78
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-01-12 13:59:58 +0530

    fixed ui issues

--- client/pages/user/dashboard.vue
+++ client/pages/user/dashboard.vue
@@ -4,15 +4,21 @@
 			<p class="title-text" dark>
 				Create short links quickly
 				<svg
+					data-v-55d53dfe=""
 					viewBox="0 0 24 24"
-					width="28"
-					height="28"
+					width="26"
+					height="26"
+					stroke="currentColor"
 					stroke-width="2"
+					fill="currentColor"
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					style="vertical-align: middle"
 				>
-					<polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
+					<polygon
+						data-v-55d53dfe=""
+						points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"
+					></polygon>
 				</svg>
 			</p>
 			<v-sheet elevation="0" class="pa-10 mt-4" rounded="xl">
