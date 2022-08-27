3b3cec75ff0c28d4d9c5f1651e6c0316de08ee95
sumitkolhe <thesumitkolhe@gmail.com>
2022-08-27 21:39:26 +0530

    fix: db connection caching

--- src/server/helpers/mongoose.helper.ts
+++ src/server/helpers/mongoose.helper.ts
@@ -9,19 +9,19 @@ let isConnected: mongoose.ConnectionStates
 export default async () => {
   try {
     if (isConnected) {
-      logger.info('=> using existing database connection')
+      logger.info('connected using cached db')
       return Promise.resolve()
     }
 
-    logger.info('=> using new database connection')
-    return await mongoose
-      .connect(config.database.dbUrl, {
-        bufferCommands: false,
-      })
-      .then((db) => {
-        isConnected = db.connections[0].readyState
-      })
-  } catch (err) {
-    console.error('DB connection failed.', err)
+    logger.info('connected using new db connection')
+    const connection = await mongoose.connect(config.database.dbUrl, {
+      keepAlive: true,
+    })
+
+    isConnected = connection.connections[0].readyState
+
+    return isConnected
+  } catch (error) {
+    console.error('database connection failed.', error)
   }
 }
