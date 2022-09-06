7172171dc422b200518b2214afb8722d4d3af231
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-06 18:18:40 +0530

    fix: auth route

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
-import { IndexRoute } from './routes/index.route'
+import { AuthRoute } from 'routes/auth.route'
 import { App } from './app'
 
-const app = new App([new IndexRoute()])
+const app = new App([new AuthRoute()])
 
 export default app.getServer()
