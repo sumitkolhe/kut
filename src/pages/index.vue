4b2c307c41184f92dac472c10e45e543c2b1fd00
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-10 11:29:39 +0530

    feat: default page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const ctx = useNuxtApp()
+
+const logout = () => {
+  ctx.$auth.logout()
+}
+</script>
 
 <template>
-  <div class="text-xl">hello</div>
+  <div class="text-xl">
+    hello
+
+    <button @click="logout">logout</button>
+  </div>
 </template>
