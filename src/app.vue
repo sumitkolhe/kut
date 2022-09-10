afabb1eca3022f8cf6a04208d1f7f1506a95e642
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-10 11:29:06 +0530

    fix: replace app.vue with layouts

--- src/app.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="text-xl">hello</div>
-</template>
---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style>
+* {
+  font-family: 'Inter', sans-serif !important;
+}
+</style>
