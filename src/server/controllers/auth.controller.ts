777b54a8e47ac816d366a012b56445d7d777fba1
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-10 15:37:31 +0530

    fix: explicitly return response

--- src/server/controllers/auth.controller.ts
+++ src/server/controllers/auth.controller.ts
@@ -4,49 +4,49 @@ import type { NextFunction, Request, RequestHandler, Response } from 'express'
 export class AuthController {
   private authService = new AuthService()
 
-  public login: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public login: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { email, password } = req.body
 
       const { accessToken, refreshToken } = await this.authService.login({ email, password })
 
-      res.json({ status: 'SUCCESS', message: null, data: { accessToken, refreshToken } })
+      return res.json({ status: 'SUCCESS', message: null, data: { accessToken, refreshToken } })
     } catch (error) {
       next(error)
     }
   }
 
-  public register: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public register: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { firstName, lastName, email, password } = req.body
 
       await this.authService.register({ firstName, lastName, email, password })
 
-      res.json({ status: 'SUCCESS', message: 'user registered successfully', data: null })
+      return res.json({ status: 'SUCCESS', message: 'user registered successfully', data: null })
     } catch (error) {
       next(error)
     }
   }
 
-  public me: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public me: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { email } = req.auth
 
       const userDetails = await this.authService.me(email)
 
-      res.json({ status: 'SUCCESS', message: null, data: userDetails })
+      return res.json({ status: 'SUCCESS', message: null, data: userDetails })
     } catch (error) {
       next(error)
     }
   }
 
-  public refreshToken: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public refreshToken: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { refreshToken } = req.body
 
       const { accessToken } = await this.authService.refreshToken(refreshToken)
 
-      res.json({ status: 'SUCCESS', message: null, data: { accessToken } })
+      return res.json({ status: 'SUCCESS', message: null, data: { accessToken } })
     } catch (error) {
       next(error)
     }
