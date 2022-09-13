e1e9a900d7c54d06c9d386f17c8cee6820335143
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-13 20:07:55 +0530

    feat: layouts

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,28 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import Navigation from 'components/molecules/navigation.vue'
+import Header from '../components/organisms/header.vue'
+
+definePageMeta({
+  middleware: 'auth',
+})
+</script>
 
 <template>
   <main>
-    <slot />
+    <Header />
+    <Navigation />
+    <div class="max-w-screen-xl mx-auto">
+      <slot />
+    </div>
   </main>
 </template>
 
 <style>
 * {
   font-family: 'Inter', sans-serif !important;
+}
+
+body {
+  @apply bg-gray-50;
 }
 </style>
---
+++ src/layouts/public.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style>
+* {
+  font-family: 'Inter', sans-serif !important;
+}
+</style>
