5fe849b0365f5e944b83d597182f71e1bf823d78
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-13 20:08:18 +0530

    fix: use auth store for logout

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,11 +1,9 @@
 import { useJwt } from '@vueuse/integrations/useJwt'
-
-const clearStorageAndRedirect = () => {
-  localStorage.clear()
-  return navigateTo('/login')
-}
+import { useAuthStore } from 'store/auth.store'
 
 export default defineNuxtRouteMiddleware(() => {
+  const AuthStore = useAuthStore()
+
   // if not server, localStorage is not accessible so return
   if (!process.client) return
 
@@ -13,13 +11,13 @@ export default defineNuxtRouteMiddleware(() => {
   const encodedJwt = localStorage.getItem('trym.access_token')
 
   // if no access token found, redirect user to login page
-  if (!encodedJwt) return clearStorageAndRedirect()
+  if (!encodedJwt) return AuthStore.logout()
 
   // if access token is found, decode it and check expiry
   const { payload } = useJwt(encodedJwt)
 
   // if token expired, redirect to login page
-  if (payload.value!.exp! < Date.now() / 1000) return clearStorageAndRedirect()
+  if (payload.value!.exp! < Date.now() / 1000) return AuthStore.logout()
 
   return
 })
