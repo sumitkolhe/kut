5550096efa4af7a79e8df2a8b631e91acd2a518e
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-13 10:24:26 +0530

    feat: atomic components

---
+++ src/components/atoms/base-button.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <button
+    :disabled="props.disabled || props.loading"
+    type="button"
+    class="inline-flex w-full disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-400 disabled:border-gray-200 cursor-pointer tracking-wide select-none appearance-none items-center justify-center rounded-md border border-gray-200 bg-black px-3 py-2 text-white transition hover:border-black hover:text-black hover:bg-white focus:outline-none active:bg-gray-100"
+    v-bind="$attrs"
+  >
+    <Icon v-if="loading" name="line-md:loading-twotone-loop" size="24" />
+    <p class="pl-4"><slot /></p>
+  </button>
+</template>
---
+++ src/components/molecules/base-input.vue
@@ -0,0 +1,68 @@
+<script lang="ts" setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: '',
+    required: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+  errors: {
+    type: Array,
+    default: () => [],
+    required: false,
+  },
+  prefixIcon: {
+    type: String,
+    default: '',
+    required: false,
+  },
+  suffixIcon: {
+    type: String,
+    default: '',
+    required: false,
+  },
+  modelValue: String || Number,
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const updateValue = (event: any) => {
+  emit('update:modelValue', event.target.value)
+}
+</script>
+
+<template>
+  <div class="w-full bg-transparent space-y-1 flex flex-col px-0 border-none py-0">
+    <label class="text-sm text-gray-500">{{ props.label }}</label>
+    <div class="relative">
+      <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2.5">
+        <Icon :name="props.prefixIcon" size="20" class="text-gray-600" />
+      </div>
+      <input
+        v-bind="$attrs"
+        :value="modelValue"
+        :disabled="props.disabled"
+        :class="[
+          props.errors.length > 0
+            ? 'border-red-500 placeholder:text-red-500 placeholder:opacity-50 focus:border-red-500 focus:ring-red-500'
+            : 'border-gray-200 placeholder:text-gray-400 focus:border-black focus:ring-black ',
+          props.prefixIcon ? 'pl-9' : '',
+          props.suffixIcon ? 'pr-9' : '',
+        ]"
+        class="block w-full rounded text-sm transition disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
+        @input="updateValue"
+      />
+
+      <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2.5">
+        <Icon :name="props.suffixIcon" size="20" class="text-gray-600" />
+      </div>
+    </div>
+    <div class="text-xs text-red-500 tracking-normal font-light h-2">
+      <p v-show="props.errors.length > 0">{{ props.errors[0] }}</p>
+    </div>
+  </div>
+</template>
