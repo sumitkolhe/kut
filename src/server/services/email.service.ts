0f8d38f9af7e9cfc6d282da45d7a49fd6a371ee9
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-15 00:41:37 +0530

    feat: add email service

---
+++ src/server/services/email.service.ts
@@ -0,0 +1,31 @@
+import { SendSmtpEmail, TransactionalEmailsApi, TransactionalEmailsApiApiKeys } from '@sendinblue/client'
+import { logger } from 'utils/logger'
+import { useConfig } from 'configs'
+import { signAccountVerificationToken } from 'helpers/token.helper'
+import type { Config } from 'interfaces/config.interface'
+
+export class EmailService {
+  public emailClient: SendSmtpEmail
+  public apiInstance: TransactionalEmailsApi
+  public emailConfig: Config['email']
+
+  constructor() {
+    this.emailConfig = useConfig().email
+    this.emailClient = new SendSmtpEmail()
+    this.apiInstance = new TransactionalEmailsApi()
+    this.apiInstance.setApiKey(TransactionalEmailsApiApiKeys.apiKey, this.emailConfig.apiKey)
+  }
+
+  public sendVerificationEmail = async (email: string, name: string): Promise<void> => {
+    const emailVerificationToken = await signAccountVerificationToken({ email })
+
+    this.emailClient.templateId = 2
+    this.emailClient.sender = { name: this.emailConfig.senderName, email: this.emailConfig.senderEmail }
+    this.emailClient.to = [{ email, name }]
+    this.emailClient.params = { VERIFICATION_LINK: `https://trym.in/api/auth/verify?token=${emailVerificationToken}` }
+
+    await this.apiInstance.sendTransacEmail(this.emailClient).catch((error) => {
+      logger.error('error sending verification email', error)
+    })
+  }
+}
