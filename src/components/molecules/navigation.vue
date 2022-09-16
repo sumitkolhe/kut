bf0e9ceb97c8e68e984034cc1708373f4ea04445
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-16 09:18:53 +0530

    fix: navigation tabs

--- src/components/molecules/navigation.vue
+++ src/components/molecules/navigation.vue
@@ -8,20 +8,20 @@ const tabs = [
 </script>
 
 <template>
-  <nav class="border-b border-gray-200 bg-white">
-    <div class="max-w-screen-xl mx-auto">
-      <ul class="flex flex-wrap space-x-8 text-sm text-center text-gray-500">
-        <NuxtLink
-          v-for="tab in tabs"
-          :key="tab.name"
-          :to="tab.href"
-          active-class="border-black border-b-[2px] text-black"
-          class="inline-flex py-4 h-12 hover:text-gray-600"
-        >
-          <Icon :name="tab.icon" size="20" class="mr-2" />
-          <p class="text-sm">{{ tab.name }}</p>
-        </NuxtLink>
-      </ul>
+  <nav class="border-b sticky top-0 dark:border-gray-700 border-gray-200 bg-white dark:bg-gray-900">
+    <div
+      class="max-w-screen-xl px-6 md:px-0 mx-auto flex flex-row space-x-8 text-gray-500 overflow-x-scroll no-scrollbar"
+    >
+      <NuxtLink
+        v-for="tab in tabs"
+        :key="tab.name"
+        :to="tab.href"
+        active-class="border-black dark:border-gray-50 border-b-[2px] text-black dark:text-gray-50"
+        class="inline-flex py-4 h-12 hover:text-gray-600 hover:dark:text-gray-300"
+      >
+        <Icon :name="tab.icon" size="20" class="mr-2" />
+        <p class="text-sm">{{ tab.name }}</p>
+      </NuxtLink>
     </div>
   </nav>
 </template>
