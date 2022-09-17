11bcc957657905ba3e49b64de1cf85b91306b2d1
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-17 21:38:58 +0530

    feat: add link router to app

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config'
 import { AuthRoute } from 'routes/auth.route'
+import { LinkRoute } from 'routes/link.route'
 import { App } from './app'
 
-const app = new App([new AuthRoute()])
+const app = new App([new AuthRoute(), new LinkRoute()])
 
 export default app.getServer()
