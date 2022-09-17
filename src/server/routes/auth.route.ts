f4d9dd94e0689354b8ac573c50aaf45656250133
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-17 21:38:26 +0530

    fix: validator import path

--- src/server/routes/auth.route.ts
+++ src/server/routes/auth.route.ts
@@ -1,63 +1,14 @@
 import { Router } from 'express'
 import { AuthController } from 'controllers/auth.controller'
 import { validate } from 'middlewares/validate.middleware'
-import { object, string, z } from 'zod'
 import { checkAuthentication } from 'middlewares/auth.middleware'
+import { loginSchema, refreshTokenSchema, regsiterationSchema, verifyAccountSchema } from 'helpers/validator.helper'
 import type { Routes } from 'interfaces/routes.interface'
-
-const regsiterationSchema = z.object({
-  body: object({
-    firstName: string({
-      required_error: 'first name is required',
-    }),
-
-    lastName: string({
-      required_error: 'last name is required',
-    }),
-
-    email: string({
-      required_error: 'email is required',
-    }).email('invalid email address'),
-
-    password: string({
-      required_error: 'password is required',
-    }).min(6, 'password too short - should be 6 chars minimum'),
-  }),
-})
-
-const loginSchema = z.object({
-  body: object({
-    email: string({
-      required_error: 'email is required',
-    }).email('invalid email address'),
-
-    password: string({
-      required_error: 'password is required',
-    }).min(6, 'password too short - should be 6 chars minimum'),
-  }),
-})
-
-const refreshTokenSchema = z.object({
-  body: object({
-    refreshToken: string({
-      required_error: 'refresh token is required',
-    }),
-  }),
-})
-
-const verifyAccountSchema = z.object({
-  query: object({
-    token: string({
-      required_error: 'token is required',
-    }),
-  }),
-})
 
 export class AuthRoute implements Routes {
   public path = '/auth'
   public router = Router()
   public authController = new AuthController()
-
   constructor() {
     this.initializeRoutes()
   }
