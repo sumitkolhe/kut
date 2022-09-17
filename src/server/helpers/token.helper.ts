d7bd28ff66a7c6c364fe8f81858b862d8e005568
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-17 21:37:42 +0530

    refactor: extract common type

--- src/server/helpers/token.helper.ts
+++ src/server/helpers/token.helper.ts
@@ -2,21 +2,23 @@ import Jwt from 'jsonwebtoken'
 import { useConfig } from 'configs'
 import type { JwtPayload } from 'jsonwebtoken'
 
+type JwtToken = string | object | Buffer
+
 const config = useConfig()
 
-export const signAccessToken = async (data: string | object | Buffer) => {
+export const signAccessToken = async (data: JwtToken) => {
   return Jwt.sign(data, config.token.access.secret, {
     expiresIn: config.token.access.expiresIn,
   })
 }
 
-export const signRefreshToken = async (data: string | object | Buffer) => {
+export const signRefreshToken = async (data: JwtToken) => {
   return Jwt.sign(data, config.token.refresh.secret, {
     expiresIn: config.token.refresh.expiresIn,
   })
 }
 
-export const signAccountVerificationToken = async (data: string | object | Buffer) => {
+export const signAccountVerificationToken = async (data: JwtToken) => {
   return Jwt.sign(data, config.token.accountVerification.secret, {
     expiresIn: config.token.accountVerification.expiresIn,
   })
