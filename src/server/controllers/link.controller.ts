dbd0c596f746f7e9a3101c36cab9fd6747bfd494
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-17 21:37:24 +0530

    feat: shorten service

---
+++ src/server/controllers/link.controller.ts
@@ -0,0 +1,37 @@
+import { LinkService } from 'services/link.service'
+import type { Link } from 'interfaces/link.interface'
+import type { NextFunction, Request, RequestHandler, Response } from 'express'
+import type { CustomResponse } from 'interfaces/response.interface'
+
+export class LinkController {
+  private linkService: LinkService
+
+  constructor() {
+    this.linkService = new LinkService()
+  }
+
+  public shorten: RequestHandler = async (req: Request, res: Response<CustomResponse<any>>, next: NextFunction) => {
+    try {
+      const { email } = req.auth
+      const {
+        alias,
+        target,
+        description,
+        meta: { password, validFrom, validTill, maxVisits } = {
+          password: null,
+          validFrom: new Date(),
+          validTill: null,
+          maxVisits: null,
+        },
+      }: Link = req.body
+
+      const link: Partial<Link> = { alias, target, description, meta: { password, validFrom, validTill, maxVisits } }
+
+      const shortenedLink = await this.linkService.shorten(email, link)
+
+      return res.json({ status: 'SUCCESS', message: null, data: shortenedLink })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
