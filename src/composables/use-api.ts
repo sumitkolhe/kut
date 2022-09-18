475a39311febaee73284a4833d7825a504de930e
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-18 17:43:49 +0530

    fix: useApi baseUrl

--- src/composables/use-api.ts
+++ src/composables/use-api.ts
@@ -1,8 +1,11 @@
 import { $fetch } from 'ohmyfetch'
 
+const baseURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api' : 'https://trym.in/api'
+
 export const useApi = $fetch.create({
-  baseURL: '/api',
+  baseURL,
   async onRequest({ options }) {
+    if (typeof window === 'undefined' || !window.localStorage) return
     const token = localStorage.getItem('trym.access_token')
 
     if (token) {
