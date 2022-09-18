4a72c7839188eb30ecea57662fdd804a70190d74
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-18 17:44:52 +0530

    feat: catch all for redirection link

---
+++ src/pages/[alias].vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'public',
+  middleware: 'redirect',
+})
+</script>
+
+<template>
+  <div class="text-xl">
+    <p>Redirection page</p>
+  </div>
+</template>
---
+++ src/pages/protected/[alias].vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'public',
+})
+</script>
+
+<template>
+  <div class="text-xl">
+    <p>Protected page</p>
+  </div>
+</template>
