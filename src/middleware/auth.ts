f2083d72797bc8be2fe683f47a58083b63915a62
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-20 00:57:03 +0530

    fix: auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,23 +1,17 @@
 import { useJwt } from '@vueuse/integrations/useJwt'
 import { useAuthStore } from 'store/auth.store'
 
-export default defineNuxtRouteMiddleware(() => {
-  const AuthStore = useAuthStore()
+export default defineNuxtRouteMiddleware(async () => {
+  const { logout } = useAuthStore()
+  const cookie = useState<string>('accessToken')
 
-  // if not server, localStorage is not accessible so return
-  if (!process.client) return
-
-  // get access token from localStorage
-  const encodedJwt = localStorage.getItem('trym.access_token')
-
-  // if no access token found, redirect user to login page
-  if (!encodedJwt) return AuthStore.logout()
+  if (!cookie.value) return await logout()
 
   // if access token is found, decode it and check expiry
-  const { payload } = useJwt(encodedJwt)
+  const { payload } = useJwt(cookie.value)
 
   // if token expired, redirect to login page
-  if (payload.value!.exp! < Date.now() / 1000) return AuthStore.logout()
+  if (payload.value!.exp! < Date.now() / 1000) return await logout()
 
   return
 })
