ded7cb93384a66b8bce576960e8537f15191cc05
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-20 01:00:43 +0530

    fix: add fetch user btn

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,17 +1,21 @@
 <script lang="ts" setup>
 import { useAuthStore } from 'store/auth.store'
-
 definePageMeta({
   middleware: 'auth',
 })
 
 const AuthStore = useAuthStore()
+
+const user = computed(() => AuthStore.user)
 </script>
 
 <template>
   <div class="p-6">
     <p>Protected page</p>
-    <button @click="AuthStore.fetchUser()">logout</button>
+    <button @click="AuthStore.fetchUser()">fetch user</button>
+    <button class="block border border-red-500" @click="AuthStore.logout()">logout</button>
     <!-- <NuxtLink to="/">Go to public page</NuxtLink> -->
+
+    <p>{{ user }}</p>
   </div>
 </template>
