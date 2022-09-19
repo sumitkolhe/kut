c41f64ab0174e6d5ef09e1e560b7868d64e3f98c
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-20 01:14:28 +0530

    fix: baseUrl for useApi

--- src/composables/use-api.ts
+++ src/composables/use-api.ts
@@ -1,5 +1,3 @@
 import { $fetch } from 'ohmyfetch'
 
-const baseURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api' : 'https://trym.in/api'
-
-export const useApi = $fetch.create({ baseURL, credentials: 'include' })
+export const useApi = $fetch.create({ baseURL: '/api', credentials: 'include' })
