9a6e5e7bfc5c901e2162628b1e11f38996fc2489
sumitkolhe <thesumitkolhe@gmail.com>
2022-09-20 00:55:32 +0530

    fix: remove localstorage interceptor

--- src/composables/use-api.ts
+++ src/composables/use-api.ts
@@ -2,14 +2,4 @@ import { $fetch } from 'ohmyfetch'
 
 const baseURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api' : 'https://trym.in/api'
 
-export const useApi = $fetch.create({
-  baseURL,
-  async onRequest({ options }) {
-    if (typeof window === 'undefined' || !window.localStorage) return
-    const token = localStorage.getItem('trym.access_token')
-
-    if (token) {
-      options.headers = { Authorization: `Bearer ${token}` }
-    }
-  },
-})
+export const useApi = $fetch.create({ baseURL, credentials: 'include' })
