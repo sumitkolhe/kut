8b88534233c127c3a61acd7e602245621929d2ef
sumitkolhe <thesumitkolhe@gmail.com>
2022-10-15 21:43:40 +0530

    fix: alias redirect middleware

--- src/middleware/redirect.ts
+++ src/middleware/redirect.ts
@@ -1,18 +1,21 @@
 import type { CustomResponse } from 'interfaces/response.interface'
 
+// redirect to external page using alias
 export default defineNuxtRouteMiddleware(async () => {
   const route = useRoute()
   const config = useRuntimeConfig()
+  const nuxtApp = useNuxtApp()
 
   const baseURL = config.apiBaseUrl
 
+  let link
   try {
-    if (!route.params.alias) return await navigateTo('/')
-
-    const link = await useRequest<CustomResponse<null>>(`${baseURL}/link/${route.params.alias}`)
-
-    return await navigateTo(link.data, { external: true, redirectCode: 301 })
+    link = await $fetch<CustomResponse<null>>(`${baseURL}/api/link/${route.params.alias}`)
+    return navigateTo(link.data, { external: true, redirectCode: 301 })
   } catch {
+    link = null
+  }
+  if (!link) {
     return navigateTo('/')
   }
 })
