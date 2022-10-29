90790eab37973e4c889ca850bd20d6025c3b7541
sumitkolhe <thesumitkolhe@gmail.com>
2022-10-29 16:47:25 +0530

    refactor: use sib api instead of client

--- src/server/services/email.service.ts
+++ src/server/services/email.service.ts
@@ -1,39 +1,40 @@
-import {
-  SendSmtpEmail,
-  TransactionalEmailsApi,
-  TransactionalEmailsApiApiKeys,
-} from '@sendinblue/client'
 import { logger } from 'server/utils/logger'
 import { useConfig } from 'server/configs'
 import { signAccountVerificationToken } from 'server/helpers/token.helper'
+import { $fetch } from 'ohmyfetch'
 import type { Config } from 'interfaces/config.interface'
 
 export class EmailService {
-  public emailClient: SendSmtpEmail
-  public apiInstance: TransactionalEmailsApi
   public emailConfig: Config['email']
 
   constructor() {
     this.emailConfig = useConfig().email
-    this.emailClient = new SendSmtpEmail()
-    this.apiInstance = new TransactionalEmailsApi()
-    this.apiInstance.setApiKey(TransactionalEmailsApiApiKeys.apiKey, this.emailConfig.apiKey)
   }
 
   public sendVerificationEmail = async (email: string, name: string): Promise<void> => {
     const emailVerificationToken = await signAccountVerificationToken({ email })
 
-    this.emailClient.templateId = 2
-    this.emailClient.sender = {
-      name: this.emailConfig.senderName,
-      email: this.emailConfig.senderEmail,
-    }
-    this.emailClient.to = [{ email, name }]
-    this.emailClient.params = {
-      VERIFICATION_LINK: `https://trym.in/api/auth/verify?token=${emailVerificationToken}`,
-    }
+    await $fetch('https://api.sendinblue.com/v3/smtp/email', {
+      retry: 2,
+      headers: {
+        Accept: 'application/json',
+        'api-key': this.emailConfig.apiKey,
+        'content-type': 'application/json',
+      },
 
-    await this.apiInstance.sendTransacEmail(this.emailClient).catch((error) => {
+      body: {
+        templateId: 2,
+        sender: {
+          name: this.emailConfig.senderName,
+          email: this.emailConfig.senderEmail,
+        },
+        to: [{ email, name }],
+        params: {
+          VERIFICATION_LINK: `https://trym.in/api/auth/verify?token=${emailVerificationToken}`,
+        },
+      },
+      method: 'POST',
+    }).catch((error) => {
       logger.error('error sending verification email', error)
     })
   }
