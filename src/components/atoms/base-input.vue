e80231b5264d503cef01d9cb92b843a41c5e493c
sumitkolhe <thesumitkolhe@gmail.com>
2022-10-30 22:01:06 +0530

    fix: base input v-model

--- src/components/atoms/base-input.vue
+++ src/components/atoms/base-input.vue
@@ -1,8 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps({
+  modelValue: String || Number,
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const updateValue = (event: any) => emit('update:modelValue', event.target.value)
+</script>
+
 <template>
   <input
-    id="link"
+    :value="props.modelValue"
     type="text"
     placeholder="Placeholder"
     class="w-full dark:bg-gray-900 dark:border-gray-700 dark:focus:border-gray-400 dark:placeholder:text-gray-500 dark:text-gray-200 placeholder:text-gray-300 placeholder:font-light block w-full rounded border-gray-200 border focus:ring-0 text-sm transition focus:border-gray-900 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
+    @input="updateValue"
   />
 </template>
