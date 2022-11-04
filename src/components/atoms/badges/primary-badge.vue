6782e6d99f36195fcdad095240983d128bf40d6f
sumitkolhe <thesumitkolhe@gmail.com>
2022-11-04 23:34:36 +0530

    feat: badge component

---
+++ src/components/atoms/badges/primary-badge.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <p
+    class="p-2 text-sm tracking-wide bg-gray-100 border rounded dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+  >
+    <slot></slot>
+  </p>
+</template>
