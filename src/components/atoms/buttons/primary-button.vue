0f611662c259611516ea96ec9e07b20b5fad5faf
sumitkolhe <thesumitkolhe@gmail.com>
2022-11-05 21:14:56 +0530

    fix: button loading

--- src/components/atoms/buttons/primary-button.vue
+++ src/components/atoms/buttons/primary-button.vue
@@ -22,9 +22,12 @@ const props = defineProps({
 
 <template>
   <button
-    :disabled="loading"
+    :disabled="props.loading"
     type="button"
-    :class="[props.prefixIcon ? 'pl-10' : '', props.suffixIcon ? 'pr-10' : '']"
+    :class="[
+      props.prefixIcon && !props.loading ? 'pl-10' : '',
+      props.suffixIcon && !props.loading ? 'pr-10' : '',
+    ]"
     class="relative inline-flex items-center justify-center w-full px-4 py-2 tracking-wide transition bg-gray-900 border border-gray-900 rounded appearance-none cursor-pointer select-none text-gray-50 dark:border-gray-50 dark:bg-gray-50 dark:text-gray-900 hover:border-gray-900 hover:text-gray-900 dark:hover:text-gray-50 hover:bg-gray-50 dark:hover:bg-gray-900 dark:hover:border-gray-50 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:dark:bg-gray-800 disabled:text-gray-300 disabled:dark:text-gray-400 disabled:border-gray-200 disabled:dark:border-gray-700"
     v-bind="$attrs"
   >
---
+++ src/components/molecules/link-card.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import Tooltip from 'components/atoms/tooltip.vue'
+import { useTimeAgo } from '@vueuse/core'
+
+const props = defineProps({
+  target: {
+    type: String,
+    default: () => '',
+    required: false,
+  },
+  shortUrl: {
+    type: String,
+    default: () => '',
+    required: false,
+  },
+  visitCount: {
+    type: Number,
+    default: () => 0,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: () => null,
+    required: false,
+  },
+  updatedAt: {
+    type: Date,
+    default: () => null,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <div
+    class="grid items-center grid-cols-1 gap-6 p-4 last:rounded-b first:rounded-t sm:grid-cols-2 lg:grid-cols-4 dark:bg-gray-900 bg-gray-50"
+  >
+    <div>
+      <NuxtLink
+        target="_blank"
+        :to="props.target"
+        class="text-sm font-medium text-red-500 truncate hover:underline"
+      >
+        {{ props.target }}
+      </NuxtLink>
+
+      <NuxtLink
+        target="_blank"
+        :to="props.shortUrl"
+        class="flex items-center mt-2 text-sm text-gray-400"
+      >
+        <Tooltip text="Shortened Link">
+          <span class="truncate hover:underline">{{ props.shortUrl }}</span>
+        </Tooltip>
+      </NuxtLink>
+    </div>
+    <div class="flex items-center space-x-2">
+      <p class="text-xl dark:text-gray-200">{{ props.visitCount }}</p>
+      <p class="text-gray-500">views</p>
+    </div>
+
+    <div>
+      <p class="text-sm text-gray-500">
+        Created
+        <span>{{ useTimeAgo(props.createdAt).value }}</span>
+      </p>
+      <p class="mt-2 text-sm text-gray-500">
+        Updated
+        <span>{{ useTimeAgo(props.updatedAt).value }}</span>
+      </p>
+    </div>
+
+    <div>
+      <Tooltip text="hello">
+        <Icon name="ic:twotone-lock" />
+      </Tooltip>
+    </div>
+  </div>
+</template>
