b50731d6b12ee1db40589978a9a1fff688414a99
sumitkolhe <thesumitkolhe@gmail.com>
2022-11-05 16:20:39 +0530

    fix: shorten link ui

--- src/components/molecules/shorten-link.vue
+++ src/components/molecules/shorten-link.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
 import PrimaryButton from 'components/atoms/buttons/primary-button.vue'
 import SecondaryButton from 'components/atoms/buttons/secondary-button.vue'
-import BaseInput from 'components/atoms/inputs/base-input.vue'
-import { Dialog, DialogPanel, Switch, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import TextInput from 'components/atoms/inputs/text-input.vue'
+import PrimaryToggle from 'components/atoms/toggles/primary-toggle.vue'
+import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import { useLinkStore } from 'store/link.store'
 import useValidate from 'vue-tiny-validate'
 import type { Rules } from 'vue-tiny-validate'
@@ -13,7 +14,6 @@ const LinkStore = useLinkStore()
 
 // refs
 const showAdvancedOptions = ref(false)
-const enabled = ref(false)
 const loading = ref(false)
 const advancedOptions = ref(false)
 
@@ -88,11 +88,12 @@ const shortenLink = async () => {
       <!-- Link input field  -->
       <div class="flex flex-col w-full space-x-0 space-y-4 md:space-x-2 md:space-y-0 md:flex-row">
         <div class="flex w-full space-x-2">
-          <base-input
+          <text-input
             v-model="linkPayload.target"
             :errors="result.target.$messages"
             prefix-icon="ph:link-simple"
             placeholder="Enter a url..."
+            @keyup.enter="shortenLink"
           />
 
           <secondary-button class="w-fit h-fit" @click="openAdvancedOptions">
@@ -111,9 +112,9 @@ const shortenLink = async () => {
       </div>
     </div>
 
-    <TransitionRoot as="template" :show="advancedOptions">
+    <transition-root as="template" :show="advancedOptions">
       <Dialog as="div" class="relative z-[999999999]">
-        <TransitionChild
+        <transition-child
           as="template"
           enter="ease-in-out duration-500"
           enter-from="opacity-50"
@@ -123,126 +124,85 @@ const shortenLink = async () => {
           leave-to="opacity-0"
         >
           <div class="fixed inset-0 transition-opacity bg-gray-800 bg-opacity-75" />
-        </TransitionChild>
+        </transition-child>
 
-        <div
-          class="fixed bottom-0 rounded-t-lg md:inset-0 shadow-xl bg-opacity-30 overflow-y-auto max-h-[75%] md:max-h-screen flex"
+        <transition-child
+          as="template"
+          enter="transform transition ease-in-out duration-300"
+          enter-from="translate-y-full md:translate-x-full md:translate-y-0"
+          enter-to="translate-y-0 md:translate-x-0 md:translate-y-0"
+          leave="transform transition ease-in-out duration-300"
+          leave-from="translate-y-0 md:translate-x-0 md:translate-y-0"
+          leave-to="translate-y-full md:translate-x-full md:translate-y-0"
         >
-          <TransitionChild
-            as="template"
-            enter="transform transition ease-in-out duration-500"
-            enter-from="md:-translate-y-6 translate-y-full"
-            enter-to="translate-y-0 md:translate-y-0"
-            leave="transform transition ease-in-out duration-500"
-            leave-from="translate-y-0 md:translate-y-0"
-            leave-to="md:translate-y-6 translate-y-full"
+          <dialog-panel
+            class="fixed flex flex-col justify-between overflow-y-auto max-h-[75%] h-full md:max-h-screen w-screen md:w-[50%] lg:w-[33%] md:right-0 bottom-0 bg-gray-50 dark:bg-gray-900 rounded-t-lg md:rounded-t-none md:border-t-0 border-t border-l-0 md:border-l dark:border-gray-700"
           >
-            <DialogPanel
-              class="bottom-0 w-screen p-6 md:m-12 lg:m-auto lg:w-fit md:rounded-lg md:p-8 h-fit md:inset-0 bg-gray-50 dark:text-gray-200 dark:bg-gray-900"
+            <div class="p-4 space-y-1 md:p-6">
+              <p class="font-medium text-gray-900 dark:text-gray-200">Advance Options</p>
+              <p class="text-sm text-gray-500">Customize link parameters</p>
+            </div>
+
+            <div class="flex flex-col p-4 space-y-6 md:p-6">
+              <div class="">
+                <text-input v-model="linkPayload.alias" label="Alias" placeholder="Enter alias" />
+              </div>
+              <div class="">
+                <text-input
+                  v-model="linkPayload.description"
+                  label="Description"
+                  placeholder="Enter description"
+                />
+              </div>
+              <div class="">
+                <text-input
+                  v-model="linkPayload.meta.password"
+                  label="Password"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div class="">
+                <text-input
+                  v-model="linkPayload.meta.maxVisits"
+                  label="Max. Visits"
+                  placeholder="Enter maximum no. of visits"
+                />
+              </div>
+              <div class="">
+                <text-input
+                  v-model="linkPayload.meta.validFrom"
+                  label="Valid From"
+                  type="datetime-local"
+                  placeholder="Enter valid from date"
+                />
+              </div>
+              <div class="">
+                <text-input
+                  v-model="linkPayload.meta.validTill"
+                  label="Valid Till"
+                  type="datetime-local"
+                  placeholder="Enter valid till date"
+                />
+              </div>
+              <div class="flex justify-between">
+                <p class="text-sm text-gray-500">Disable Link</p>
+                <primary-toggle v-model="linkPayload.meta.active"></primary-toggle>
+              </div>
+            </div>
+            <div
+              class="bottom-0 flex flex-row items-center justify-end w-full p-4 space-x-4 border-t dark:border-gray-700 md:p-6"
             >
-              <div class="mb-4 space-y-1">
-                <p class="font-medium text-gray-900 dark:text-gray-200">Advance Options</p>
-                <p class="text-sm text-gray-500">Customize link parameters</p>
-              </div>
-
-              <div class="relative grid grid-cols-1 lg:grid-cols-2 gap-x-10">
-                <div class="flex items-center py-3">
-                  <base-input v-model="linkPayload.alias" label="Alias" placeholder="Enter alias" />
-                </div>
-                <div class="flex items-center justify-between py-2">
-                  <base-input
-                    v-model="linkPayload.description"
-                    label="Description"
-                    placeholder="Enter description"
-                  />
-                </div>
-                <div class="flex items-center justify-between py-2">
-                  <base-input
-                    v-model="linkPayload.meta.password"
-                    label="Password"
-                    placeholder="Enter password"
-                  />
-                </div>
-                <div class="flex items-center justify-between py-2">
-                  <base-input
-                    v-model="linkPayload.meta.maxVisits"
-                    label="Max. Visits"
-                    placeholder="Enter maximum no. of visits"
-                  />
-                </div>
-                <div class="flex items-center py-2">
-                  <base-input
-                    v-model="linkPayload.meta.validFrom"
-                    label="Valid From"
-                    type="datetime-local"
-                    placeholder="Enter valid from date"
-                  />
-                </div>
-                <div class="flex items-center justify-between py-2">
-                  <base-input
-                    v-model="linkPayload.meta.validTill"
-                    label="Valid Till"
-                    type="datetime-local"
-                    placeholder="Enter valid till date"
-                  />
-                </div>
-                <div class="flex items-center justify-between py-3">
-                  <p class="text-sm text-gray-500">Disable Link</p>
-                  <span class="ml-auto">
-                    <Switch
-                      v-model="enabled"
-                      :class="[
-                        enabled ? 'bg-red-500' : 'bg-gray-300',
-                        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none',
-                      ]"
-                    >
-                      <span
-                        :class="[
-                          enabled ? 'translate-x-5' : 'translate-x-0',
-                          'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
-                        ]"
-                      >
-                        <span
-                          :class="[
-                            enabled
-                              ? 'opacity-0 ease-out duration-100'
-                              : 'opacity-100 ease-in duration-200',
-                            'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
-                          ]"
-                          aria-hidden="true"
-                        >
-                          <icon name="ph:x-bold" class="text-gray-400" size="10" />
-                        </span>
-                        <span
-                          :class="[
-                            enabled
-                              ? 'opacity-100 ease-in duration-200'
-                              : 'opacity-0 ease-out duration-100',
-                            'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
-                          ]"
-                          aria-hidden="true"
-                        >
-                          <icon name="ph:check-bold" class="text-red-500" size="10" />
-                        </span>
-                      </span>
-                    </Switch>
-                  </span>
-                </div>
-              </div>
-
-              <div class="flex flex-row items-end justify-end mt-6 space-x-4">
-                <secondary-button class="w-auto" @click="closeAdvancedOptions">
-                  Cancel
-                </secondary-button>
-                <primary-button class="w-auto px-8" @click="saveAdvancedOptions">
-                  Save
-                </primary-button>
-              </div>
-            </DialogPanel>
-          </TransitionChild>
-        </div>
+              <secondary-button class="w-auto" @click="closeAdvancedOptions">
+                Cancel
+              </secondary-button>
+              <primary-button class="w-auto px-8" @click="saveAdvancedOptions">
+                Save
+              </primary-button>
+            </div>
+          </dialog-panel>
+        </transition-child>
       </Dialog>
-    </TransitionRoot>
+    </transition-root>
   </div>
 </template>
 
