5c387604adea88c9cb7e8cafcada6d724bf28cab
sumitkolhe <thesumitkolhe@gmail.com>
2022-11-06 09:09:18 +0530

    feat: add toggle component

---
+++ src/components/atoms/toggles/primary-toggle.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: () => false,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const updateValue = (e: Event) => emit('update:modelValue', (e.target as HTMLInputElement).value)
+</script>
+
+<template>
+  <div
+    class="flex items-center h-8 p-1 duration-300 bg-gray-300 rounded-full cursor-pointer w-14"
+    :class="{ 'bg-green-500': props.modelValue }"
+    @click="updateValue"
+  >
+    <div
+      class="w-6 h-6 duration-300 transform bg-white rounded-full shadow-md"
+      :class="{ 'translate-x-6': props.modelValue }"
+    ></div>
+  </div>
+</template>
