c12e2a56751ef18681fee18b6560d700c78b3a50
sumitkolhe <thesumitkolhe@gmail.com>
2022-11-06 19:11:22 +0530

    fix: remove page transitions

--- src/components/molecules/drawer.vue
+++ src/components/molecules/drawer.vue
@@ -7,36 +7,18 @@ const { isDrawerVisible } = useDrawer()
 
 <template>
   <teleport to="body">
-    <transition name="slide-fade">
-      <aside
-        v-show="isDrawerVisible"
-        class="flex-col md:hidden z-[999999] flex h-screen w-full fixed bg-gray-50 dark:bg-gray-900"
-      >
-        <div class="h-16 dark:bg-gray-900 bg-gray-50">header</div>
+    <aside
+      v-show="isDrawerVisible"
+      class="flex-col md:hidden z-[999999] flex h-screen w-full fixed bg-gray-50 dark:bg-gray-900"
+    >
+      <div class="h-16 dark:bg-gray-900 bg-gray-50">header</div>
 
-        <section class="flex h-screen border-t dark:border-gray-700">
-          <div class="flex flex-col w-full p-4 space-y-3">
-            <secondary-button>Login</secondary-button>
-            <primary-button>Register</primary-button>
-          </div>
-        </section>
-      </aside>
-    </transition>
+      <section class="flex h-screen border-t dark:border-gray-700">
+        <div class="flex flex-col w-full p-4 space-y-3">
+          <secondary-button>Login</secondary-button>
+          <primary-button>Register</primary-button>
+        </div>
+      </section>
+    </aside>
   </teleport>
 </template>
-
-<style scoped>
-.slide-fade-enter-active {
-  transition: all 0.3s;
-}
-
-.slide-fade-leave-active {
-  transition: all 0.3s;
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateX(20px);
-  opacity: 0;
-}
-</style>
--- src/components/molecules/shorten-link.vue
+++ src/components/molecules/shorten-link.vue
@@ -209,19 +209,3 @@ const shortenLink = async () => {
     </transition-root>
   </div>
 </template>
-
-<style scoped>
-.slide-fade-enter-active {
-  transition: all 0.3s;
-}
-
-.slide-fade-leave-active {
-  transition: all 0.3s;
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateY(-20px);
-  opacity: 0;
-}
-</style>
