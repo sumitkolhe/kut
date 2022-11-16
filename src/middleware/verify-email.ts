c6e42dc7fb76e9db6fb8df902ca15d341b787ba0
sumitkolhe <thesumitkolhe@gmail.com>
2022-11-16 22:06:04 +0530

    refactor: middlewares

--- src/middleware/verify-email.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-  if (process.client) {
-    const { $auth } = useNuxtApp()
-    const isEmailVerified = computed(() => $auth.user.value.isVerified)
-
-    if (isEmailVerified.value === false) return navigateTo('/auth/verify-email')
-  }
-})
