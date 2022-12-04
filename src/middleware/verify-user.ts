580e64dfeeff7e0179e7266fd26907593aeedc9b
sumitkolhe <thesumitkolhe@gmail.com>
2022-12-04 13:34:55 +0530

    fix: use router instead of navigate To

--- src/middleware/verify-user.ts
+++ src/middleware/verify-user.ts
@@ -2,14 +2,15 @@ import type { User } from 'interfaces/user.interface'
 
 export default defineNuxtRouteMiddleware(async () => {
   const { $auth } = useNuxtApp()
+  const router = useRouter()
 
   try {
     const user = $auth.$storage.getStore()?.user?.data as User
 
     const isVerified = user?.isVerified || false
 
-    if (!user) return navigateTo('/')
-    else if (user && isVerified === false) return navigateTo('/dashboard/verify')
+    if (!user) return router.replace('/')
+    else if (user && isVerified === false) return router.replace('/dashboard/verify')
   } catch (error) {
     if (error instanceof Error) logger.error(error.message)
   }
