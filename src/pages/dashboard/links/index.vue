055010c9782f5682a34381e398ae77a2cbd6d131
sumitkolhe <thesumitkolhe@gmail.com>
2023-01-09 20:03:50 +0530

    feat: add links skeleton loader

--- src/pages/dashboard/links/index.vue
+++ src/pages/dashboard/links/index.vue
@@ -19,13 +19,16 @@ definePageMeta({
 const { fetchAllLinks, deleteLink, shortenLink } = useLinkStore()
 const { totalCount, allLinks } = storeToRefs(useLinkStore())
 
-onMounted(() => {
-  fetchAllLinks(0, 5)
+onMounted(async () => {
+  linksLoader.value = true
+  await fetchAllLinks(0, 5)
+  linksLoader.value = false
 })
 
 // refs
 const showLinkPanel = ref(false)
 const loading = ref(false)
+const linksLoader = ref(false)
 const search = ref()
 const qrCode = ref()
 const openQrCode = ref(false)
@@ -44,8 +47,10 @@ const linkPayload = reactive<Pick<Link, 'alias' | 'target' | 'meta' | 'descripti
 })
 
 // functions
-const debouncedSearch = useDebounceFn(() => {
-  fetchAllLinks(0, 5, search.value)
+const debouncedSearch = useDebounceFn(async () => {
+  linksLoader.value = true
+  await fetchAllLinks(0, 5, search.value)
+  linksLoader.value = false
 }, 500)
 
 const createLink = async () => {
@@ -68,14 +73,16 @@ const { currentPage, pageCount, prev, next, isFirstPage, isLastPage } = useOffse
   total: totalCount,
   page: 1,
   pageSize: 5,
-  onPageChange: ({
+  onPageChange: async ({
     currentPage,
     currentPageSize,
   }: {
     currentPage: number
     currentPageSize: number
   }) => {
-    fetchAllLinks((currentPage - 1) * 5, currentPageSize)
+    linksLoader.value = true
+    await fetchAllLinks((currentPage - 1) * 5, currentPageSize)
+    linksLoader.value = false
   },
 })
 
@@ -135,55 +142,75 @@ watch(copied, (clicked) => {
       ></create-link-panel>
     </div>
 
-    <div v-if="allLinks.length > 0">
-      <p class="mt-6 py-3 font-medium uppercase text-gray-600 dark:text-gray-200">My Links</p>
+    <div v-if="linksLoader" class="my-6 flex w-full flex-col gap-6 md:my-8">
+      <div
+        v-for="i in [1, 2, 3, 4]"
+        :key="i"
+        class="w-full animate-pulse space-y-2 rounded-md border bg-gray-50 p-5"
+      >
+        <div class="flex-1 space-y-6">
+          <div class="grid grid-cols-3 gap-4">
+            <div class="col-span-1 h-2 rounded-full bg-gray-300"></div>
+          </div>
 
-      <div class="space-y-3">
-        <link-card
-          v-for="link in allLinks.slice(0, 5)"
-          :key="link.alias?.toString()"
-          :alias="link.alias"
-          :description="link.description"
-          :target="link.target"
-          :short-url="link.shortUrl"
-          :visit-count="link.visitCount"
-          :created-at="link.createdAt.toString()"
-          :updated-at="link.updatedAt.toString()"
-          @copy-link="copy(link.shortUrl)"
-          @delete-link="deleteSelectedLink(link.alias)"
-          @edit-link="editSelectedLink(link.alias)"
-          @qr-code="showQrCode"
-        />
-      </div>
-
-      <qr-code-panel
-        v-if="qrCode"
-        :key="qrCode"
-        :is-open="openQrCode"
-        :link="qrCode"
-        @close="openQrCode = false"
-      />
-
-      <div class="flex justify-center md:justify-end">
-        <div class="mt-6 mb-0 flex items-end items-center justify-end gap-3">
-          <icon-button :disabled="isFirstPage" icon="line-md:chevron-left" @click="prev" />
-          <p class="text-sm dark:text-gray-400">
-            {{ currentPage }}
-            <span class="mx-0.25">/</span>
-            {{ pageCount }}
-          </p>
-          <icon-button :disabled="isLastPage" icon="line-md:chevron-right" @click="next" />
+          <div class="col-span-1 h-2 rounded-full bg-gray-300"></div>
         </div>
       </div>
     </div>
 
-    <div
-      v-else
-      class="relative my-6 flex h-64 flex-col items-center justify-center rounded border bg-gray-50 px-4 py-6 dark:border-gray-700 dark:bg-gray-900"
-    >
-      <icon name="ph:link-break-duotone" size="44" class="opacity-50 dark:text-gray-400" />
-      <p class="mt-4 mb-1 font-medium dark:text-gray-200">No Links</p>
-      <p class="font-light text-gray-400 dark:text-gray-500">Get started by creating a new link.</p>
+    <div v-else>
+      <div v-if="allLinks.length > 0">
+        <p class="mt-6 py-3 font-medium uppercase text-gray-600 dark:text-gray-200">My Links</p>
+
+        <div class="space-y-3">
+          <link-card
+            v-for="link in allLinks.slice(0, 5)"
+            :key="link.alias?.toString()"
+            :alias="link.alias"
+            :description="link.description"
+            :target="link.target"
+            :short-url="link.shortUrl"
+            :visit-count="link.visitCount"
+            :created-at="link.createdAt.toString()"
+            :updated-at="link.updatedAt.toString()"
+            @copy-link="copy(link.shortUrl)"
+            @delete-link="deleteSelectedLink(link.alias)"
+            @edit-link="editSelectedLink(link.alias)"
+            @qr-code="showQrCode"
+          />
+        </div>
+
+        <qr-code-panel
+          v-if="qrCode"
+          :key="qrCode"
+          :is-open="openQrCode"
+          :link="qrCode"
+          @close="openQrCode = false"
+        />
+
+        <div class="flex justify-center md:justify-end">
+          <div class="mt-6 mb-0 flex items-end items-center justify-end gap-3">
+            <icon-button :disabled="isFirstPage" icon="line-md:chevron-left" @click="prev" />
+            <p class="text-sm dark:text-gray-400">
+              {{ currentPage }}
+              <span class="mx-0.25">/</span>
+              {{ pageCount }}
+            </p>
+            <icon-button :disabled="isLastPage" icon="line-md:chevron-right" @click="next" />
+          </div>
+        </div>
+      </div>
+
+      <div
+        v-else
+        class="relative my-6 flex h-64 flex-col items-center justify-center rounded border bg-gray-50 px-4 py-6 dark:border-gray-700 dark:bg-gray-900"
+      >
+        <icon name="ph:link-break-duotone" size="44" class="opacity-50 dark:text-gray-400" />
+        <p class="mt-4 mb-1 font-medium dark:text-gray-200">No Links</p>
+        <p class="font-light text-gray-400 dark:text-gray-500">
+          Get started by creating a new link.
+        </p>
+      </div>
     </div>
   </section>
 </template>
