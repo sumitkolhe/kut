48bde177d04fc9a37e33f567f15b885fcc222635
sumitkolhe <thesumitkolhe@gmail.com>
2023-02-04 15:27:39 +0530

    fix: remove feature points

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,26 +2,4 @@ export default defineAppConfig({
   nuxtIcon: {
     size: '24px',
   },
-  createAccountPoints: [
-    {
-      title: 'Free Account',
-      description:
-        'Create apps, connect databases and add-on services, and collaborate on your apps, for free.',
-    },
-    {
-      title: 'Your app platform',
-      description:
-        'A platform for apps, with app management & instant scaling, for development and production.',
-    },
-    {
-      title: 'Deploy now',
-      description:
-        'Go from code to running app in minutes. Deploy, scale, and deliver your app to the world.',
-    },
-    {
-      title: 'Free account',
-      description:
-        'Create apps, connect databases and add-on services, and collaborate on your apps, for free.',
-    },
-  ],
 })
--- src/pages/auth/register.vue
+++ src/pages/auth/register.vue
@@ -3,6 +3,7 @@ import PrimaryButton from 'components/atoms/buttons/primary-button.vue'
 import TextInput from 'components/atoms/inputs/text-input.vue'
 import useValidate from 'vue-tiny-validate'
 import { useAuthStore } from 'store/auth.store'
+import PasswordInput from 'components/atoms/inputs/password-input.vue'
 import type { Rules } from 'vue-tiny-validate'
 
 definePageMeta({
@@ -11,7 +12,6 @@ definePageMeta({
 })
 
 const { registerUser } = useAuthStore()
-const { createAccountPoints } = useAppConfig()
 
 const loading = ref(false)
 const registerData = reactive({ email: '', password: '' })
@@ -42,14 +42,14 @@ const register = async () => {
 </script>
 
 <template>
-  <section class="flex min-h-screen justify-center bg-gray-100 dark:bg-gray-800">
+  <section class="flex min-h-screen items-center justify-center bg-gray-100 dark:bg-gray-800">
     <!-- Inner container  -->
-    <div class="flex max-w-screen-2xl flex-col lg:flex-row">
+    <div class="flex h-fit w-full max-w-xl flex-col rounded-lg dark:border-gray-700 lg:flex-row">
       <!-- Register Form  -->
-      <div
-        class="flex min-h-screen w-full flex-col items-center justify-center space-y-8 px-6 md:mt-0 md:h-full md:p-10 lg:w-1/2 xl:p-24"
-      >
-        <h1 class="mb-4 text-2xl font-medium dark:text-gray-200 xl:text-3xl">Create Account</h1>
+      <div class="flex w-full flex-col items-center justify-center space-y-8 px-6 md:p-10 xl:p-14">
+        <h1 class="mb-4 text-2xl font-medium tracking-wide dark:text-gray-200 xl:text-3xl">
+          Create Account
+        </h1>
         <!-- <div
           class="flex flex-col items-center justify-center w-full space-y-4 md:flex-row md:space-y-0 md:space-x-4"
         >
@@ -71,52 +71,31 @@ const register = async () => {
           </p>
         </div> -->
 
-        <form class="w-full space-y-4">
-          <!-- <base-input label="First Name" type="text" placeholder="john" suffix-icon="ph:at" />
-              <base-input label="Last Name" type="text" placeholder="doe" suffix-icon="ph:at" /> -->
-          <text-input
-            v-model="registerData.email"
-            label="Email"
-            type="email"
-            placeholder="john@doe.com"
-            suffix-icon="ph:at"
-            :errors="result.email.$messages"
-          />
-          <text-input
-            v-model="registerData.password"
-            label="Password"
-            type="password"
-            placeholder="********"
-            suffix-icon="ph:password"
-            :errors="result.password.$messages"
-          />
-          <primary-button :loading="loading" @click="register">Sign up</primary-button>
+        <form class="flex w-full flex-col items-center space-y-8">
+          <div class="w-full space-y-4">
+            <text-input
+              v-model="registerData.email"
+              label="Email"
+              type="email"
+              placeholder="john@doe.com"
+              suffix-icon="ph:at"
+              :errors="result.email.$messages"
+            />
+            <password-input
+              v-model="registerData.password"
+              label="Password"
+              placeholder="********"
+              suffix-icon="ph:password"
+              :errors="result.password.$messages"
+            />
+          </div>
+
+          <primary-button :loading="loading" @click="register"> Log in </primary-button>
         </form>
         <p class="text-center text-gray-600">
           Already have an account?
           <nuxt-link to="/auth/login" class="text-red-500 hover:underline">Log in</nuxt-link>
         </p>
-      </div>
-      <!-- Features  -->
-      <div
-        class="flex w-full flex-col items-start justify-center space-y-8 rounded-tr-lg rounded-br-lg bg-gray-100 px-6 py-12 dark:bg-gray-800 md:p-10 lg:w-1/2 xl:p-16"
-      >
-        <div
-          v-for="feature in createAccountPoints"
-          :key="feature.title"
-          class="flex items-start justify-start space-x-3"
-        >
-          <Icon name="ph:check-circle-duotone" size="32" class="text-red-500" />
-
-          <div class="space-y-1">
-            <p class="text-left text-lg font-medium text-gray-900 dark:text-gray-200">
-              {{ feature.title }}
-            </p>
-            <p class="text-left text-gray-900 dark:text-gray-300">
-              {{ feature.description }}
-            </p>
-          </div>
-        </div>
       </div>
     </div>
   </section>
