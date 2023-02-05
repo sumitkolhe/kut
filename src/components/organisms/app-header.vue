a65ebdc31ad395dfe5ae49921af1e20d2c99f1ae
sumitkolhe <thesumitkolhe@gmail.com>
2023-02-05 11:26:16 +0530

    fix: blur on dark mode

--- src/components/organisms/app-header.vue
+++ src/components/organisms/app-header.vue
@@ -12,7 +12,11 @@ const { y } = useWindowScroll()
 
 <template>
   <header
-    :class="[y >= 10 ? 'border-b bg-opacity-50 backdrop-blur transition-all duration-200' : '']"
+    :class="[
+      y >= 10
+        ? 'border-b bg-opacity-50 backdrop-blur transition-all duration-200 dark:bg-opacity-50'
+        : '',
+    ]"
     class="fixed z-50 w-full bg-gray-50 px-6 dark:border-gray-700 dark:bg-gray-900"
   >
     <div class="mx-auto flex h-16 max-w-screen-xl flex-row items-center justify-between">
