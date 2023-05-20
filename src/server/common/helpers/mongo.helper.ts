e324e48cfc346e0e0be25ef261bd635b67c281a6
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2023-05-20 23:09:47 +0530

    feat: indexes

--- src/server/common/helpers/mongo.helper.ts
+++ src/server/common/helpers/mongo.helper.ts
@@ -20,8 +20,25 @@ export default async () => {
     cachedClient = await MongoClient.connect(config.database.dbUrl)
 
     const db = cachedClient.db(config.database.dbName)
+
     papr.initialize(db)
+
     await papr.updateSchemas()
+
+    await db.collection('users').createIndex({ email: 1 }, { unique: true })
+    await db.collection('links').createIndex({ alias: 1 }, { unique: true })
+    await db.collection('links').createIndex(
+      {
+        alias: 'text',
+        target: 'text',
+        shortUrl: 'text',
+        description: 'text',
+      },
+      {
+        name: 'text_search_index',
+      }
+    )
+
     isConnected = true
 
     logger.info('connected using new db connection')
