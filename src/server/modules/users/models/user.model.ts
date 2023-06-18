a33f9e1b8747016e953248ffce15223c543e055e
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2023-06-18 16:37:25 +0530

    fix: disable id creation for sub schemas

--- src/server/modules/users/models/user.model.ts
+++ src/server/modules/users/models/user.model.ts
@@ -1,4 +1,5 @@
-import { getModelForClass, prop } from '@typegoose/typegoose'
+import { getModelForClass, modelOptions, prop } from '@typegoose/typegoose'
+import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
 
 class UserProfile {
   @prop({ type: String, required: false, lowercase: true })
@@ -11,54 +12,67 @@ class UserProfile {
   public picture?: string
 }
 
+@modelOptions({
+  schemaOptions: {
+    _id: false,
+  },
+})
 class SocialAuthProviders {
   @prop({ type: Boolean, required: true, default: false })
   public google?: boolean
 
   @prop({ type: Boolean, required: true, default: false })
   public github?: boolean
+
+  @prop({ type: Boolean, required: true, default: false })
+  public credentials?: boolean
 }
 
+@modelOptions({
+  schemaOptions: {
+    _id: false,
+  },
+})
 class UserApiKeys {
-  @prop({ type: String, required: false, index: true })
+  @prop({ type: String, required: true, index: true, default: '9876hvkyvifiuui-jvyviucut' })
   public key?: string
 
   @prop({ type: Date, required: true, default: Date.now })
   public issuedOn?: Date
 
-  @prop({ type: Date, required: true })
+  @prop({ type: Date, required: false, default: null })
   public expirationDate?: Date
 
-  @prop({ type: String, required: true })
+  @prop({ type: String, required: true, default: 'api-key' })
   public name?: string
 
-  @prop({ type: Date, required: false })
+  @prop({ type: Date, required: false, default: null })
   public lastUsedOn?: Date
 }
 
-class UserClass {
+class UserClass extends TimeStamps {
   @prop({ type: UserProfile })
   public profile?: UserProfile
 
   @prop({ type: SocialAuthProviders })
   public authProviders?: SocialAuthProviders
 
+  @prop({ type: UserApiKeys })
+  public apiKeys?: UserApiKeys
+
   @prop({ type: String, required: true, lowercase: true, unique: true, index: true })
   public email!: string
 
   @prop({ type: String, required: false, minlength: 6, select: false })
-  public password!: string
+  public password?: string
 
   @prop({ type: Boolean, required: false, default: false })
   public isBanned?: boolean
 
   @prop({ type: Boolean, required: false, default: false })
   public isVerified?: boolean
-
-  @prop({ type: [UserApiKeys], required: false })
-  public apiKeys?: UserApiKeys[]
 }
 
-const UserModel = getModelForClass(UserClass)
+const UserModel = getModelForClass(UserClass, { schemaOptions: { collection: 'users' } })
 
 export { UserModel, UserClass }
